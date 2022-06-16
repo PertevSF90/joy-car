0326f1a8815073be762d4cb2170df42cc60fc328
Pertev <66652035+PertevSF90@users.noreply.github.com>
2022-06-16 16:16:49 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 function links () {
     if (JoyCar.linefinder(SensorLCRSelection.Left)) {
         JoyCar.turn(
-        FRDirection.Forward,
+        FRDirection.Reverse,
         LRDirection.Left,
         100,
         1
@@ -18,7 +18,7 @@ function links () {
 function rechts () {
     if (JoyCar.obstacleavoidance(SensorLRSelection.Right)) {
         JoyCar.turn(
-        FRDirection.Forward,
+        FRDirection.Reverse,
         LRDirection.Right,
         100,
         1
@@ -42,19 +42,24 @@ function halt () {
         . # # # .
         `)
 }
+JoyCar.light(ToggleSwitch.On)
+JoyCar.brakelight(ToggleSwitch.On)
 basic.forever(function () {
+    JoyCar.drive(FRDirection.Forward, 20)
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
     if (JoyCar.obstacleavoidance(SensorLRSelection.Right)) {
-        links()
-    } else if (JoyCar.obstacleavoidance(SensorLRSelection.Left)) {
         rechts()
-    } else {
-        JoyCar.drive(FRDirection.Forward, 20)
-        basic.showLeds(`
-            . . . . .
-            # . . . #
-            . # . # .
-            . . # . .
-            . . . . .
-            `)
+    }
+    if (JoyCar.obstacleavoidance(SensorLRSelection.Left)) {
+        links()
+    }
+    for (let Index2 = 50; Index2 >= JoyCar.sonar(); Index2--) {
+        halt()
     }
 })
